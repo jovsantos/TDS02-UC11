b235a37dac9dce3299ee078a14d0afcd86490bd6
João Santos <contatojoaosantos01@gmail.com>
2026-06-18 20:27:47 -0300

    Adicionando arquivos do projeto

---
+++ TDS02-UC11/IntroducaoTestes/src/UtilitariosApp/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace UtilitariosApp
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ TDS02-UC11/IntroducaoTestes/src/UtilitariosApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using UtilitariosApp;
+
+Calculadora c1 = new Calculadora();
+
+var resultado = c1.Somar(14, 17);
+var resultado2 = c1.Multiplicar(14, 17);
+
+
+Console.WriteLine($"O resultado da soma é: {resultado}");
+Console.WriteLine($"O resultado da multiplicação é: {resultado2}");
