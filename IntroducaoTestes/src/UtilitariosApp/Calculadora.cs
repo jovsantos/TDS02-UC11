7f27f30a903fd2d558590ca98dcd5a61332d5100
João Santos <contatojoaosantos01@gmail.com>
2026-06-18 22:03:54 -0300

    Implementando Testes de Operações

--- IntroducaoTestes/src/UtilitariosApp/Calculadora.cs
+++ IntroducaoTestes/src/UtilitariosApp/Calculadora.cs
@@ -11,5 +11,18 @@
         {
             return a * b;
         }
+
+        public int Divisao(double a, double b)
+        {
+            if (b == 0)
+            {
+                return (int)a; // Retorna o dividendo se o divisor for zero
+            }
+
+            var resultado = a / b;
+            return (int)Math.Ceiling(resultado);
+        }
+
+
     }
 }
---
+++ IntroducaoTestes/tests/UtilitariosApp.Tests/CalculadoraTests.cs
@@ -0,0 +1,47 @@
+﻿using UtilitariosApp;
+
+namespace UtilitariosApp.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact] // Indica que este método é um teste
+        public void Somar_DeveRetornarASomaCorreta()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Somar(5, 7);
+            var resultadoZerado = calculadora.Somar(0, 0); // Teste para verificar a soma de zero, serve para garantir que a função lida corretamente com casos de borda
+
+            Assert.Equal(12, resultado);
+            Assert.Equal(0, resultadoZerado); // Verifica se a soma de zero é igual a zero
+        }
+
+        [Fact]
+        public void Multiplicar_DeveRetornarAMultiplicacaoCorreta()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Multiplicar(3, 3); // 3 * 3 = 9
+
+            Assert.Equal(9, resultado); // Verifica se o resultado é igual a 9
+
+        }
+
+        [Fact]
+        public void Dividir_DeveRetornaroResultadoInteiroDaDivisaoArredondadoParaCima()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Divisao(5, 2); // 5 / 2 = 2.5, porem , como o método deve retornar um inteiro arredondado para cima, o resultado esperado é 3
+
+            Assert.Equal(3, resultado); 
+        }
+
+        [Fact]
+        public void Dividir_AoDividirPorZero_DeveRetornarODividendo()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Divisao(5, 0); // 5 / 0 = indefinido
+
+            Assert.Equal(5, resultado);
+        }
+
+    }
+}
